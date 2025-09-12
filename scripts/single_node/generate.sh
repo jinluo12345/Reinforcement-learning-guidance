45285bfb806c605f23ec56c9d61df29ea3b0828f
jinluo <jin040227@qq.com>
2025-09-12 19:56:20 +0000

    fix generage.sh

--- scripts/single_node/generate.sh
+++ scripts/single_node/generate.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=0 /inspire/hdd/project/embodied-multimodality/public/lzjjin/anaconda3/envs/rlg/bin/python /inspire/hdd/project/embodied-multimodality/public/lzjjin/Reinforcement-learning-guidance/scripts/generate.py \
+python scripts/generate.py \
   --config="config/dgx.py:pickscore_sd3" \
-  --lora_path="/inspire/hdd/project/embodied-multimodality/public/lzjjin/Flow-RLG/model-pretrained/flow_grpo/models--jieliu--SD3.5M-FlowGRPO-PickScore/snapshots/10c56697459bbdbe54d5e375912f49a0bcfae773" \
-  --tuned_guidance_scale=2.4 \
-  --output_dir="logs/aes_generated_images/" \
-  --prompt_file="/inspire/hdd/project/embodied-multimodality/public/lzjjin/Reinforcement-learning-guidance/dataset/pickscore/test.txt"
+  --lora_path="{your downloaded RL-finetuned model's ckpt path}" \
+  --tuned_guidance_scale=1.6 \
+  --output_dir="logs/" \
+  --prompt_file="dataset/pickscore/test.txt"
